f35ebb64805ab2ceb2bf7a0857cd4a56a7e24205
royalcat <k.adamovich20@gmail.com>
2025-04-17 14:22:51 +0400

    navbar margins

--- ui/modules/navbar_templ.go
+++ ui/modules/navbar_templ.go
@@ -56,7 +56,7 @@ func Navbar() templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<nav class=\"border-b py-3\"><div class=\"container mx-auto px-4 flex justify-between items-center\"><div class=\"flex items-center space-x-4\"><div class=\"text-sm text-gray-600 dark:text-gray-400 font-mono\">")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<nav class=\"border-b py-3\"><div class=\"flex justify-between items-center mx-16\"><div class=\"flex items-center space-x-4\"><div class=\"text-sm text-gray-600 dark:text-gray-400 font-mono\">")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
